eb1e7590fcd5daba4e6e421e12009376bed71fcf
ndirangu <ndirangu.muchai@gmail.com>
2021-05-08 14:22:42 +0300

    added getItemById unit test

--- starter_code/src/test/java/com/example/demo/controllers/ItemControllerTest.java
+++ starter_code/src/test/java/com/example/demo/controllers/ItemControllerTest.java
@@ -12,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 public class ItemControllerTest {
@@ -24,17 +26,19 @@ public class ItemControllerTest {
     ItemController itemController;
 
     List<Item> items;
+    Item item;
+    Item itemIngine;
     @Before
     public void setUp(){
         MockitoAnnotations.initMocks(this);
 
-        Item item = new Item();
+        item = new Item();
         item.setId(1L);
         item.setName("itemYaKwanza");
         item.setDescription("ki description tu");
         item.setPrice(BigDecimal.valueOf(12));
 
-        Item itemIngine = new Item();
+        itemIngine = new Item();
         itemIngine.setId(2L);
         itemIngine.setName("itemYaPili");
         itemIngine.setDescription("ki description tu");
@@ -58,5 +62,19 @@ public class ItemControllerTest {
         assertNotNull(testList);
         assertArrayEquals(items.toArray(), testList.toArray());
     }
-    
+
+    @Test
+    public void getItemById() {
+        when(itemRepository.findById(anyLong())).thenReturn(Optional.of(item));
+
+        ResponseEntity<Item> itemResponseEntity = itemController.getItemById(1L);
+        Item savedItem = itemResponseEntity.getBody();
+
+        assertNotNull(itemResponseEntity);
+        assertEquals(200, itemResponseEntity.getStatusCodeValue());
+
+        assertNotNull(savedItem);
+        assertEquals("ki description tu", savedItem.getDescription());
+        assertEquals(BigDecimal.valueOf(12), savedItem.getPrice());
+    }
 }
